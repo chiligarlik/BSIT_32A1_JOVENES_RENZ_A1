f10bfb48040acc8d6e566b4fec77ee495bd4e84d
Renz Louie E. Jovenes <162081730+chiligarlik@users.noreply.github.com>
2025-02-08 09:33:12 +0800

    S

---
+++ scripts.js
@@ -0,0 +1,4 @@
+
+document.getElementById('hobbiesBtn').addEventListener('click', function() {
+    alert('My hobbies include playing basketball, going to the gym, listening to music and drinking coffee');
+});
